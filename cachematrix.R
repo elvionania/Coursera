5740851f14f73971b58b8757a8f8e9fc192dbfe8
Elvio <naniaelvio@gmail.com>
2016-12-04 09:17:56 -0500

    inverse matrix caching implementation

--- cachematrix.R
+++ cachematrix.R
@@ -1,26 +1,35 @@
 
-makeCacheMatrix <- function(x = matrix()) {
-  m<-NULL
-  set<-function(y){
-  x<<-y
-  m<<-NULL
-}
-get<-function() x
-setmatrix<-function(solve) m<<- solve
-getmatrix<-function() m
-list(set=set, get=get,
-   setmatrix=setmatrix,
-   getmatrix=getmatrix)
+cachesolve<-function(x,...){
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
 
-cacheSolve <- function(x=matrix(), ...) {
-    m<-x$getmatrix()
-    if(!is.null(m)){
-      message("getting cached data")
-      return(m)
-    }
-    datos<-x$get()
-    m<-solve(datos, ...)
-    x$setmatrix(m)
-    m
+makeCacheMatrix<-function(x){
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(matrice) i <<- matrice
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+isRegularMatrice<-function(x){
+  if(!is.matrix(x)){
+    return(0)
+  }else if(ncol(x)!=nrow(x)){
+    return(0)
+  }
+  return(1)
 }
